b50a6fc0d84e40539aca8f30cf411e0d34d064de
landeralvin <landeralvin@gmail.com>
2020-07-10 15:58:59 +0700

    fix config.json and datasets/preprocess.py

--- datasets/preprocess.py
+++ datasets/preprocess.py
@@ -205,3 +205,15 @@ def custom_turkish(root_path, meta_file):
             items.append([text, wav_file, speaker_name])
     print(f" [!] {len(skipped_files)} files skipped. They don't exist...")
     return items
+def javalg(root_path, meta_file):
+    """Normalizes the Nancy meta data file to TTS format"""
+    txt_file = os.path.join(root_path, meta_file)
+    items = []
+    speaker_name = "java-female"
+    with open(txt_file, 'r') as ttf:
+        for line in ttf:
+            cols = line.split('\t')
+            wav_file = os.path.join(root_path, 'wavs', cols[0] + '.wav')
+            text = cols[1]
+            items.append([text, wav_file, speaker_name])
+    return items
